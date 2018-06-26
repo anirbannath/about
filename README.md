809ae88255d885d64f5beac10fa20270a4ea37c4
Anirban Nath <ani.djrocker@gmail.com>
2018-06-27 00:53:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# anirbannath.website
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+(function () {
+  // initiating particles
+  particlesJS.load('app-particles', './scripts/particles.config.json', function () {
+    console.log('particles.js loaded - callback');
+  });
+
+}());
