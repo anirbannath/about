ed11b0bfb70ab5e17c50bdceecfbed444468f79a
Anirban Nath <ani.djrocker@gmail.com>
2021-08-20 11:14:26 +0530

    Updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 (function () {
   particlesJS.load('app-particles', './scripts/particles.config.json');
-  document.getElementById('footer-author').innerHTML = '&copy; Anirban Nath ' + new Date(Date.now()).getFullYear()
+  document.getElementById('years-of-experience').innerHTML = new Date(Date.now()).getFullYear() - 2016;
+  document.getElementById('trainer-experience').innerHTML = new Date(Date.now()).getFullYear() - 2017;
+  document.getElementById('footer-author').innerHTML = '&copy; Anirban Nath 2017 - ' + new Date(Date.now()).getFullYear()
 }());
